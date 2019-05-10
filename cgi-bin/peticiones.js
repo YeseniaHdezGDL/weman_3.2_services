36a44f2783b653de22720bf9537ed8118f8eb47f
Yesenia <hernandezy1603@gmail.com>
2019-05-10 16:04:03 -0500

    cambié los nombres de algunas variables, la request se hace desde url y marca un error en cmder, pero si lo corre en el navegador, aunque cada vez que se hace un request, se tiene que actualizar cmder

--- cgi-bin/peticiones.js
+++ cgi-bin/peticiones.js
@@ -1,30 +1,30 @@
-
-const svr = require('http');
-const usr = require('url');
+const servidor = require('http');
+const url = require('url');
 const fibo = require('./fibonacci.js');
+
 
 //Aquí hay que jalar los 'require' adicionales que puedan hacer falta como FileSystem, etc.
 const host = '127.0.0.1';
 const puerto = '8070';
 
-const servidor = elServidor.createServer((pet, resp) => { //
-	let respuesta = '';
-	resp.setHeader('Content-Type', 'text/plain');
-	if (pet.method == 'GET') {
-		//respuesta = procesaGet(pet);
+const svr = servidor.createServer((requerimiento, respuesta) => {
+	let resultado = '';
+	respuesta.setHeader('Content-Type', 'text/plain');
+	if (requerimiento.method == 'GET') {
+		resultado = procesaGet(requerimiento);
 		console.log("Una petición");
-		console.log(fibo.doFibonacci(10));
-		resp.statusCode = 200;
-	} else if (pet.method == 'POST') {
-		respuesta = procesaPost(pet);
-		resp.statusCode = 200;
+		console.log(fibo.doFibonacci(10000)); //aquí estoy pidiendo los primeros 10 números de la seq fibonacci
+		respuesta.statusCode = 200;
+	} else if (requerimiento.method == 'POST') {
+		respuesta = procesaPost(requerimiento);
+		respuesta.statusCode = 200;
 	} else {
-		resp.statusCode = 404;
+		respuesta.statusCode = 404;
 	}
-	resp.end(respuesta);
+	respuesta.end(resultado);
 });
 
-servidor.listen(puerto, host, () => {
+svr.listen(puerto, host, () => {
 	console.log('La aplicación está corriendo en: ' + host + ':' + puerto);
 });
 
@@ -33,8 +33,8 @@ servidor.listen(puerto, host, () => {
 function procesaGet(peticion) {
 	//Aquí necesitan analizar la URL de la petición, ver qué botón se presionó y actuar en consecuencia.
 var palabra = url.parse(peticion.url, true); // ESTO ES PARSEAR
-console.log(palabra);
-console.log("Posible palíndroma = " + palabra.query.texto)
+console.log("Posible palíndroma = " + palabra.query.numero);
+return fibo.doFibonacci(parseInt(palabra.query.numero)).toString();
 }
 
 function procesaPost(peticion) {
